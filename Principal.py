4605b444c1147d56324843be6a8521b4849967a9
Lucas Oliveira <lucas_oliveira1205@hotmail.com>
2016-10-03 15:24:48 -0300

    Atividade - 03/09/16

---
+++ Principal.py
@@ -0,0 +1,8 @@
+import StringUtil
+
+texto= input("digite o texto aqui:")
+StringUtil.output(texto)
+StringUtil.tamanho(texto)
+StringUtil.maiusculo(texto)
+StringUtil.minusculo(texto)
+
---
+++ StringUtil.py
@@ -0,0 +1,14 @@
+def output(texto):
+    print(texto)
+def tamanho(texto):
+    tamanhotexto= len(texto)
+    print("tamanhotexto", tamanho)
+    return tamanhotexto
+def maiusculo(texto):
+    maiusculo= texto.upper()
+    print("maiusculo", maiusculo)
+    return maiusculo
+def minusculo(texto):
+    minusculo=texto.lower()
+    print("minusculo", minusculo)
+    return minusculo
